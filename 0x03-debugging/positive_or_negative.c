5c873bbd38ff46f95ee69db366e05778a56ce69f
Ennymuzondo <muzondo.enica55@gmail.com>
2023-07-20 07:06:05 -0700

    1-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *positive or negative - print a number
+ *@i : integer
+ *if the number is greater than 0: is positive;
+ *if the number is less than a 0: is genetive, followed by a new line
+ *Return 0
+
+
+   void positive-or_negative(int i)
+{  
+	if (i > 0)
+                printf("%d is positive\n", i);
+
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}		
